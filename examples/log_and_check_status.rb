ca5366297ead354e2267b3a7d756eeef7eeaa526
Gilad Buchman <gilad.buchman@gmail.com>
2011-09-02 12:01:27 -0700

    update example

--- examples/log_and_check_status.rb
+++ examples/log_and_check_status.rb
@@ -4,12 +4,13 @@ require 'badgeville'
 require 'pp'
 
 settings = JSON.parse(File.read('keys.json'))
-badgeville = Badgeville::Client.new('test@keas.com', settings)
+badgeville = Badgeville::Client.new('me@example.com', settings)
 
-pp badgeville.reward_definitions
+pp badgeville.reward_definitions #all defined definitions for site
 
 pp badgeville.log_activity "commented"
 
 pp badgeville.log_activity "join_team", :team => "myteam"
 
 pp badgeville.get_activities
+pp badgeville.get_rewards #earned for user
