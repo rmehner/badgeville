22e8414efc0c6fa3e38072fa22c1e75fe3333130
Robin Mehner <robin@coding-robin.de>
2012-05-30 14:06:08 +0200

    Use rspec's let instead of instance variables in reward_spec

--- spec/badgeville/reward_spec.rb
+++ spec/badgeville/reward_spec.rb
@@ -1,38 +1,51 @@
 require 'spec_helper'
 
 describe Badgeville::Reward do
-  before do
-    @parsed_json = JSON.parse('{
-    "name": "A Way with Words",
-    "active_start_at": null,
-    "image_file_name": "9fQ2IU6-0.ftbs4fsoogp3c8fr.png",
-    "data": {
-      "verb": "commented",
-      "threshold": 2
-    },
-    "created_at": "2011-08-18T22:55:03-07:00",
-    "image_url": "http://s3.amazon.com/original.png?1",
-    "components": "[{\"command\":\"count\",\"comparator\":{\"$gte\":2},\"config\":{},\"where\":{\"verb\":\"commented\",\"player_id\":\"%player_id\"}}]",
-    "reward_template": {
-      "message": ""
-    },
-    "_id": "4e4dfab6c47eed727b005c38",
-    "tags": null,
-    "id": "4e4dfab6c47eed727b005c38",
-    "active_end_at": null,
-    "type": "achievement",
-    "hint": "Reply to 25 Comments",
-    "assignable": false,
-    "allow_duplicates": false,
-    "site_id": "4e4d5bf5c47eed25a0000e8f",
-    "active": true,
-    "message": "You won the internet!"
-   }')
+  let(:reward_json) do
+    {
+      'name'             => 'A Way with Words',
+      'active_start_at'  => nil,
+      'image_file_name'  => '9fQ2IU6-0.ftbs4fsoogp3c8fr.png',
+      'data'             => {
+        'verb'      => 'commented',
+        'threshold' => 2
+      },
+      'created_at' => '2011-08-18T22:55:03-07:00',
+      'image_url'  => 'http://s3.amazon.com/original.png?1',
+      'components' => [
+        {
+          'command'    => 'count',
+          'comparator' => {
+            '$gte' => 2
+          },
+          'config' => {
+          },
+          'where'=> {
+            'verb'      => 'commented',
+            'player_id' => '%player_id'
+          }
+        }
+      ],
+      'reward_template'  => {
+        'message' => ''
+      },
+      '_id'              => '4e4dfab6c47eed727b005c38',
+      'tags'             => nil,
+      'id'               => '4e4dfab6c47eed727b005c38',
+      'active_end_at'    => nil,
+      'type'             => 'achievement',
+      'hint'             => 'Reply to 25 Comments',
+      'assignable'       => false,
+      'allow_duplicates' => false,
+      'site_id'          => '4e4d5bf5c47eed25a0000e8f',
+      'active'           => true,
+      'message'          => 'You won the internet!'
+    }
   end
 
   context "when json is a reward definition" do
     before do
-      @reward = Badgeville::Reward.new(@parsed_json)
+      @reward = Badgeville::Reward.new(reward_json)
     end
 
     it "initialize based on json" do
@@ -59,15 +72,15 @@ describe Badgeville::Reward do
       end
 
       it "returns a protocol relative url for a https original image url" do
-        @parsed_json['image_url'] = 'https://s3.amazon.com/original.png?1'
-        reward = Badgeville::Reward.new(@parsed_json)
+        reward_json['image_url'] = 'https://s3.amazon.com/original.png?1'
+        reward = Badgeville::Reward.new(reward_json)
 
         reward.image_url.should == '//s3.amazon.com/original.png?1'
       end
 
       it "returns nil if the image url is nil in the parsed json" do
-        @parsed_json['image_url'] = nil
-        reward = Badgeville::Reward.new(@parsed_json)
+        reward_json['image_url'] = nil
+        reward = Badgeville::Reward.new(reward_json)
 
         reward.image_url.should be_nil
       end
@@ -102,17 +115,20 @@ describe Badgeville::Reward do
 
   context "when json is a reward earned by user" do
     before do
-      @parsed_json = {"user_id" => "1", "id" => "new_id",
-        "created_at" => "2011-08-18T22:55:03-07:00",
-        "definition" => @parsed_json}
-      @reward = Badgeville::Reward.new(@parsed_json)
+      @user_reward_json = {
+        'user_id'    => '1',
+        'id'         => 'new_id',
+        'created_at' => '2011-08-18T22:55:03-07:00',
+        'definition' => reward_json
+      }
+      @reward = Badgeville::Reward.new(@user_reward_json)
     end
 
     it "initialize based on json" do
       @reward.name.should == "A Way with Words"
       @reward.image_url.should =~ /original.png/
       @reward.active.should be_true
-      @reward.earned_at.iso8601.should == Time.parse(@parsed_json["created_at"]).iso8601
+      @reward.earned_at.iso8601.should == Time.parse(@user_reward_json["created_at"]).iso8601
       @reward.id.should == "new_id"
       @reward.definition_id.should == '4e4dfab6c47eed727b005c38'
       @reward.message.should == 'You won the internet!'
@@ -121,7 +137,7 @@ describe Badgeville::Reward do
 
   context "when reward is an achievement (has data with verb and threshold)" do
     before do
-      @reward = Badgeville::Reward.new(@parsed_json)
+      @reward = Badgeville::Reward.new(reward_json)
     end
 
     it "has verb and threshold accessors" do
