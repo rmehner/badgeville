4da7a52f810ceb600634cd62c7a70a76425e4630
Thorsten Ball <mrnugget@gmail.com>
2012-06-18 16:55:19 +0200

    Basic implementation of Badgeville::Group

---
+++ lib/badgeville/group.rb
@@ -0,0 +1,37 @@
+module Badgeville
+  class Group < Endpoint
+    ATTRIBUTES = [
+      :id, :name, :type, :image_url, :tip, :message, :privileges, :note,
+      :display_priority, :reward_image_url, :track_member, :adjustment,
+      :units_possible
+    ]
+
+    ATTRIBUTES.each do |attr|
+      attr_accessor attr
+    end
+
+    def self.find_by_site(site)
+      response = client.get_all('groups.json', {site: site})
+
+      response ? response.map {|group| new(group)} : []
+    end
+
+    def initialize(json = {})
+      @json = json
+
+      ATTRIBUTES.each do |attribute|
+        send("#{attribute}=", @json[attribute.to_s])
+      end
+    end
+
+    def rewards
+      @rewards ||= @json['reward_definitions'].map do |reward_json|
+        Reward.new(reward_json)
+      end
+    end
+
+    def image_url(format = :original)
+      @image_url.sub('original', format.to_s).sub(/^https?:/, '') if @image_url
+    end
+  end
+end
